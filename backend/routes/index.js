a9869838a13637e5d9b03a9a562d73597fb352e8
ecobarcelo417-lab <ecobarcelo417@gmail.com>
2026-08-29 12:59:24 +0800

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'CKD Management System API', version: '1.0.0' });
-});
-
-module.exports = router;
